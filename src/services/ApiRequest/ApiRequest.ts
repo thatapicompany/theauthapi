505c70a343be2b68aa211281761594e6dec73c49
Fibi <charafbahria@gmail.com>
2022-04-05 21:07:40 +0100

    add Date parsing to ApiRequest

--- src/services/ApiRequest/ApiRequest.ts
+++ src/services/ApiRequest/ApiRequest.ts
@@ -19,6 +19,7 @@ class ApiRequest implements ApiCall {
   headers: object;
   accessKey: string;
   timeout: number;
+  retryCount: number;
 
   constructor(config: Config) {
     const { host, accessKey, headers, retryCount, timeout } = config;
@@ -26,13 +27,41 @@ class ApiRequest implements ApiCall {
     this.accessKey = accessKey;
     this.headers = this._generateDefaultHeaders();
     this.timeout = timeout ? (typeof timeout === "string" ? ms(timeout) : timeout) : 0;
+    this.retryCount = retryCount ?? 3;
 
     if (headers) {
       this.headers = { ...this.headers, headers };
     }
 
+    this._init();
+  }
+
+  _init() {
+    const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?(?:[-+]\d{2}:?\d{2}|Z)?$/
+    function isIsoDateString(value: any): boolean {
+      return value && typeof value === "string" && isoDateFormat.test(value);
+    }
+    function handleDates(body: any) {
+      if (body === null || body === undefined || typeof body !== "object"){
+        return body;
+      }
+      for (const key of Object.keys(body)) {
+        const value = body[key];
+        if (isIsoDateString(value)) {
+          body[key] = new Date(value);
+        } else if (typeof value === "object") {
+          handleDates(value);
+        }
+      }
+    }
+
+    axios.interceptors.response.use((response) => {
+      handleDates(response.data);
+      return response;
+    });
+
     axiosRetry(axios, {
-      retries: retryCount ?? 3,
+      retries: this.retryCount ?? 3,
       retryCondition: this._isErrorRetryable,
       retryDelay: axiosRetry.exponentialDelay,
     });
