87d7fde37a939e851dbe9f46b3682c7755490875
Nick W Forsberg <nick@theforsbergs.co.uk>
2023-09-12 18:21:54 +0100

    Added rotate key feature

--- src/endpoints/ApiKeys/ApiKeysInterface.ts
+++ src/endpoints/ApiKeys/ApiKeysInterface.ts
@@ -8,10 +8,11 @@ import {
 export interface ApiKeysInterface {
   isValidKey(apiKey: string): Promise<boolean>;
   getKey(apiKey: string): Promise<ApiKey>;
-  authenticateKey(apiKey: string): Promise<ApiKey>
+  authenticateKey(apiKey: string): Promise<ApiKey>;
   getKeys(filter?: ApiKeyFilter): Promise<ApiKey[]>;
   createKey(apiKey: ApiKeyInput): Promise<ApiKey>;
   updateKey(apiKey: string, updateTo: UpdateApiKeyInput): Promise<ApiKey>;
   deleteKey(apiKey: string): Promise<boolean>;
   reactivateKey(apiKey: string): Promise<ApiKey>;
+  rotateKey(apiKey: string): Promise<ApiKey>;
 }
