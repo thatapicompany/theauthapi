1bc2c8fec89efd724c0389f97d06086ab15adc2a
aden forshaw <aden.forshaw@gmail.com>
2026-06-29 15:16:35 +1000

    feat: forward origin domain in authenticateKey to support domain locking

--- src/services/ApiRequest/ApiCall.ts
+++ src/services/ApiRequest/ApiCall.ts
@@ -1,7 +1,12 @@
 import { HttpMethod } from './HttpMethod';
 
 interface ApiCall {
-  request<T>(method: HttpMethod, endpoint: string, payload?: any): Promise<T>;
+  request<T>(
+    method: HttpMethod,
+    endpoint: string,
+    payload?: any,
+    customHeaders?: object,
+  ): Promise<T>;
 }
 
 export default ApiCall;
